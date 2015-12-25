95b53f2cd94f7a23479dd065b79e5737b87e0873
Danny Guinther <dannyguinther@gmail.com>
2015-12-25 16:53:47 -0500

    Use SHA256 checksum

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,5 +1,5 @@
 default.tmux.bin_path = "/opt/tmux/bin/tmux"
-default.tmux.checksum = "977871e7433fe054928d86477382bd5f6794dc3d"
+default.tmux.checksum = "795f4b4446b0ea968b9201c25e8c1ef8a6ade710ebca4657dd879c35916ad362"
 default.tmux.configure_options = %w[ --prefix=/opt/tmux ]
 default.tmux.install_method = "source"
 default.tmux.symlink_group = "root"
