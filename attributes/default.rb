c572fd31eb8c461e6194f18f01824ef13944c41d
Danny Guinther <dannyguinther@gmail.com>
2015-12-25 16:36:35 -0500

    Set attributes individually

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,13 +1,11 @@
-default.tmux = {
-  :bin_path => "/opt/tmux/bin/tmux",
-  :checksum => "977871e7433fe054928d86477382bd5f6794dc3d",
-  :configure_options => %w[ --prefix=/opt/tmux ],
-  :install_method => "source",
-  :symlink_group => "root",
-  :symlink_path => "/usr/bin/tmux",
-  :symlink_user => "root",
-  :user_conf_group => "root",
-  :user_conf_path => "/root/.tmux.conf",
-  :user_conf_user => "root",
-  :version => "2.0",
-}
+default.tmux.bin_path = "/opt/tmux/bin/tmux"
+default.tmux.checksum = "977871e7433fe054928d86477382bd5f6794dc3d"
+default.tmux.configure_options = %w[ --prefix=/opt/tmux ]
+default.tmux.install_method = "source"
+default.tmux.symlink_group = "root"
+default.tmux.symlink_path = "/usr/bin/tmux"
+default.tmux.symlink_user = "root"
+default.tmux.user_conf_group = "root"
+default.tmux.user_conf_path = "/root/.tmux.conf"
+default.tmux.user_conf_user = "root"
+default.tmux.version = "2.0"
